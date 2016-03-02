93e445d7dcb3bee474e1bb7bef6b06bd6e44f1fd
Ilya Pikulin <ispikilin@gmail.com>
2016-03-03 00:43:45 +0300

    add an installation script

---
+++ paths.py
@@ -0,0 +1,14 @@
+paths = {
+    ".astylerc": "~/",
+    ".tmux.conf": "~/",
+    ".Xdefaults": "~/",
+    ".xinitrc": "~/",
+    ".zshrc": "~/",
+    "i3.conf": "~/.i3/config",
+    "qutebrowser.conf": "~/.config/qutebrowser/",
+    "qutebrowser_keys.conf": "~/.config/qutebrowser/",
+    "resolv.conf": "/etc/",
+    "torrc": "/etc/tor/",
+    "vimrc": "/etc/",
+    "wpa_supplicant.conf": "/etc/wpa_supplicant/",
+}
